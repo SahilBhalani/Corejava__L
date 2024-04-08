f8a497ddfd40d03f9b884c9cfd45ca45f89edcc6
Sahil <sahilbhalani007@gmail.com>
2024-04-08 15:06:40 +0530

    hibernate Crud added

---
+++ HibernateWEBCrud/src/main/java/Controller/RegController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import java.io.IOException;
+
+import dao.StudentDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Student;
+
+@WebServlet("/reg")
+public class RegController extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Student st = new Student();
+		
+		String id = req.getParameter("id");
+		if(id.equals("") || id.equals(null))
+		{
+			
+		}
+		else
+		{
+			st.setId(Integer.parseInt(id));
+		}
+		
+		st.setUname(req.getParameter("uname"));
+		st.setEmail(req.getParameter("email"));
+		st.setPass(req.getParameter("pass"));
+		st.setPhone(req.getParameter("phone"));
+		
+		
+		StudentDao dao = new StudentDao();
+		int i = dao.addOrUpdate(st);
+		
+		if(id.equals("") || id.equals(null))
+		{
+			req.setAttribute("msg", "REgistration SuccessFul");
+		}
+		else
+		{
+			req.setAttribute("msg", "Update SucccessFul");
+		}
+		
+		req.getRequestDispatcher("Index.jsp").forward(req, resp);
+	}
+	
+}
---
+++ HibernateWEBCrud/src/main/java/Controller/displayController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import dao.StudentDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Student;
+
+
+@WebServlet("/display")
+public class displayController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		StudentDao dao = new StudentDao();
+		List<Student> al = dao.viewAllStd();
+		
+		req.setAttribute("data", al);
+		req.getRequestDispatcher("Display.jsp").forward(req, resp);
+		
+		
+	}
+
+}
